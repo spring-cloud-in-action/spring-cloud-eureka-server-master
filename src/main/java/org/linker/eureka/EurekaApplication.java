80c4ead8f5792883d9be6a8986b3192a2d9f5d90
nguyen <rwmingad@163.com>
2018-11-06 17:17:54 +0800

    spring-cloud-eureka-server

---
+++ src/main/java/org/linker/eureka/EurekaApplication.java
@@ -0,0 +1,14 @@
+package org.linker.eureka;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(EurekaApplication.class).web(true).run(args);
+    }
+}
