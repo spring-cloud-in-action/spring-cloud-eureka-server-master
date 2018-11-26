bd7f54ccf2caa680d468dd27b3288c7966e86bb6
RWM <rwmingad@163.com>
2018-11-26 16:33:50 +0800

    eureka-server集群模式

--- src/main/java/org/linker/eureka/EurekaServerApplication.java
+++ src/main/java/org/linker/eureka/EurekaServerApplication.java
@@ -1,14 +1,15 @@
 package org.linker.eureka;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@SpringBootApplication
 @EnableEurekaServer
-public class EurekaApplication {
+@SpringBootApplication
+public class EurekaServerApplication {
 
     public static void main(String[] args) {
-        new SpringApplicationBuilder(EurekaApplication.class).web(true).run(args);
+        SpringApplication.run(EurekaServerApplication.class, args);
     }
 }
